7aa77d8663c009c06fcb60a7d953b751e85a1653
mzaini30 <muhzaini30@gmail.com>
2024-03-23 15:22:50 +0800

    feat: todo selesai

--- TODO_files/db.js
+++ TODO_files/db.js
@@ -1,4 +1,6 @@
-alasql(`create localStorage database if not exists todo;
-attach localStorage database todo;
-use todo;
-create table if not exists tasks(id integer primary key, task text, status integer);`);
+let namaDatabase = 'todo';
+
+alasql(/*sql*/ `create localStorage database if not exists ${namaDatabase};
+attach localStorage database ${namaDatabase};
+use ${namaDatabase};
+create table if not exists todo(id integer primary key, todo text, isDone integer);`);
