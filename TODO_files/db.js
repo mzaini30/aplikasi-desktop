7f8ef0582dc6d0dec8c934efb3313013bab6282e
mzaini30 <muhzaini30@gmail.com>
2024-03-24 17:31:47 +0800

    feat: perbaikan fitur todo

--- TODO_files/db.js
+++ TODO_files/db.js
@@ -1,6 +1,6 @@
-let namaDatabase = 'todo';
+let namaDatabase = "todo";
 
 alasql(/*sql*/ `create localStorage database if not exists ${namaDatabase};
 attach localStorage database ${namaDatabase};
 use ${namaDatabase};
-create table if not exists todo(id integer primary key, todo text, isDone integer);`);
+create table if not exists todo(id integer auto_increment, todo text, isDone integer);`);
---
+++ TODO_files/global.js
@@ -0,0 +1,18 @@
+function toast(teks) {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: "success",
+    title: teks,
+  });
+}
