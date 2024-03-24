3c0d0d0790ab5a65ef5e5f6dc0e1ecaa72d3bcb0
mzaini30 <muhzaini30@gmail.com>
2024-03-24 18:05:39 +0800

    feat: catatan pakai iframe

--- Catatan_files/db.js
+++ Catatan_files/db.js
@@ -1,6 +1,6 @@
-let namaDatabase = 'catatan';
+let namaDatabase = "catatan";
 
 alasql(/*sql*/ `create localStorage database if not exists ${namaDatabase};
 attach localStorage database ${namaDatabase};
 use ${namaDatabase};
-create table if not exists catatan(id integer primary key, judul text, isi text, slug text);`);
+create table if not exists catatan(id integer auto_increment, judul text, isi text, slug text);`);
